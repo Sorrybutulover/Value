7f440171a07055cdac783e4ab46a724e91fc1834
Sorrybutulover <egor__shherbakov.03@mail.ru>
2024-06-22 18:54:46 +0300

    slider

--- src/components/Comand-slide.js
+++ src/components/Comand-slide.js
@@ -4,104 +4,148 @@ import "./../css/comand-slide.css";
 function Slide() {
   const [activeSlide, setActiveSlide] = useState(1);
 
-  // Обработчик изменения чекбокса
   const handleCheckboxChange = (slideNumber) => {
     setActiveSlide(slideNumber);
   };
 
   return (
-    <>
-      <div className="section full-height over-hide px-4 px-sm-0">
-        <div className="container">
-          <div className="row full-height justify-content-center">
-            <div className="col-lg-10 col-xl-8 align-self-center padding-tb">
-              <div className="section mx-auto text-center slider-height-padding">
-                <input
-                  className="checkbox frst"
-                  type="radio"
-                  id="slide-1"
-                  name="slider"
-                  checked={activeSlide === 1}
-                  onChange={() => handleCheckboxChange(1)}
-                />
-                <label htmlFor="slide-1"></label>
-                <input
-                  className="checkbox scnd"
-                  type="radio"
-                  id="slider-2"
-                  name="slider"
-                  checked={activeSlide === 2}
-                  onChange={() => handleCheckboxChange(2)}
-                />
-                <label htmlFor="slider-2"></label>
-                <input
-                  className="checkbox thrd"
-                  type="radio"
-                  id="slider-3"
-                  name="slider"
-                  checked={activeSlide === 3}
-                  onChange={() => handleCheckboxChange(3)}
-                />
-                <label htmlFor="slider-3"></label>
-                <input
-                  className="checkbox foth"
-                  type="radio"
-                  id="slider-4"
-                  name="slider"
-                  checked={activeSlide === 4}
-                  onChange={() => handleCheckboxChange(4)}
-                />
-                <label htmlFor="slider-4"></label>
+    <div className="section full-height over-hide px-4 px-sm-0">
+      <div className="container">
+        <div className="row full-height justify-content-center">
+          <div className="col-lg-10 col-xl-8 align-self-center padding-tb">
+            <div className="section mx-auto text-center slider-height-padding">
+              <input
+                className="checkbox frst"
+                type="radio"
+                id="slide-1"
+                name="slider"
+                checked={activeSlide === 1}
+                onChange={() => handleCheckboxChange(1)}
+              />
+              <label htmlFor="slide-1"></label>
+              <input
+                className="checkbox scnd"
+                type="radio"
+                id="slider-2"
+                name="slider"
+                checked={activeSlide === 2}
+                onChange={() => handleCheckboxChange(2)}
+              />
+              <label htmlFor="slider-2"></label>
+              <input
+                className="checkbox thrd"
+                type="radio"
+                id="slider-3"
+                name="slider"
+                checked={activeSlide === 3}
+                onChange={() => handleCheckboxChange(3)}
+              />
+              <label htmlFor="slider-3"></label>
+              <input
+                className="checkbox foth"
+                type="radio"
+                id="slider-4"
+                name="slider"
+                checked={activeSlide === 4}
+                onChange={() => handleCheckboxChange(4)}
+              />
+              <label htmlFor="slider-4"></label>
 
-
-                <ul className="ulll">
-                  <li
-                    className="slides"
-                    style={{
-                      opacity: activeSlide === 1 ? 1 : 0,
-                      pointerEvents: activeSlide === 1 ? "auto" : "none",
-                      borderRadius: activeSlide === 1 ? "16px" : "0px",
-                    }}
-                  >
-                    <span>MALE GOOFY FACE</span>
-                  </li>
-                  <li
-                    className="slides"
-                    style={{
-                      opacity: activeSlide === 2 ? 1 : 0,
-                      pointerEvents: activeSlide === 2 ? "auto" : "none",
-                      borderRadius: activeSlide === 2 ? "16px" : "0px",
-                    }}
-                  >
-                    <span>TOY PIG</span>
-                  </li>
-                  <li
-                    className="slides"
-                    style={{
-                      opacity: activeSlide === 3 ? 1 : 0,
-                      pointerEvents: activeSlide === 3 ? "auto" : "none",
-                      borderRadius: activeSlide === 3 ? "16px" : "0px",
-                    }}
-                  >
-                    <span>SHY PORTRAIT</span>
-                  </li>
-                  <li
-                    className="slides"
-                    style={{
-                      opacity: activeSlide === 4 ? 1 : 0,
-                      pointerEvents: activeSlide === 4 ? "auto" : "none",
-                      borderRadius: activeSlide === 4 ? "16px" : "0px",
-                    }}
-                  >
-                    <span>SKATEBOARD FACE</span>
-                  </li>
-                </ul>
-              </div>
+              <ul className="ulll">
+                <li
+                  className="slides"
+                  style={{
+                    opacity: activeSlide === 1 ? 1 : 0,
+                    pointerEvents: activeSlide === 1 ? "auto" : "none",
+                  }}
+                >
+                  <div className="slide-content">
+                    <div
+                      className="image"
+                      style={{
+                        backgroundImage:
+                          'url("https://assets.codepen.io/1462889/sl1.jpg")',
+                      }}
+                    ></div>
+                    <div className="text">
+                      <p>Имя: Ольга</p>
+                      <p>VK: <a href="https://vk.com" target="_blank" rel="noopener noreferrer">ссылка</a></p>
+                      <p>Телефон: 89529999999</p>
+                    </div>
+                  </div>
+                </li>
+                <li
+                  className="slides"
+                  style={{
+                    opacity: activeSlide === 2 ? 1 : 0,
+                    pointerEvents: activeSlide === 2 ? "auto" : "none",
+                  }}
+                >
+                  <div className="slide-content">
+                    <div
+                      className="image"
+                      style={{
+                        backgroundImage:
+                          'url("https://assets.codepen.io/1462889/sl2.jpg")',
+                      }}
+                    ></div>
+                    <div className="text">
+                      <p>Имя: Алексей</p>
+                      <p>VK: <a href="https://vk.com" target="_blank" rel="noopener noreferrer">ссылка</a></p>
+                      <p>Телефон: 89527777777</p>
+                    </div>
+                  </div>
+                </li>
+                <li
+                  className="slides"
+                  style={{
+                    opacity: activeSlide === 3 ? 1 : 0,
+                    pointerEvents: activeSlide === 3 ? "auto" : "none",
+                  }}
+                >
+                  <div className="slide-content">
+                    <div
+                      className="image"
+                      style={{
+                        backgroundImage:
+                          'url("https://assets.codepen.io/1462889/sl3.jpg")',
+                      }}
+                    ></div>
+                    <div className="text">
+                      <p>Имя: Мария</p>
+                      <p>VK: <a href="https://vk.com" target="_blank" rel="noopener noreferrer">ссылка</a></p>
+                      <p>Телефон: 89525555555</p>
+                    </div>
+                  </div>
+                </li>
+                <li
+                  className="slides"
+                  style={{
+                    opacity: activeSlide === 4 ? 1 : 0,
+                    pointerEvents: activeSlide === 4 ? "auto" : "none",
+                  }}
+                >
+                  <div className="slide-content">
+                    <div
+                      className="image"
+                      style={{
+                        backgroundImage:
+                          'url("https://assets.codepen.io/1462889/sl4.jpg")',
+                      }}
+                    ></div>
+                    <div className="text">
+                      <p>Имя: Дмитрий</p>
+                      <p>VK: <a href="https://vk.com" target="_blank" rel="noopener noreferrer">ссылка</a></p>
+                      <p>Телефон: 89524444444</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
